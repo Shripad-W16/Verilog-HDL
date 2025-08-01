3dee45a69a08ffe2e27b10cf22867e78291786a0
Shripad-W16 <shripadingle11@gmail.com>
2025-08-01 19:46:38 +0530

    Create Down_Counter_4bit.v

---
+++ Down_Counter_4bit.v
@@ -0,0 +1,14 @@
+module down_counter(clk,reset,count);
+    input clk;
+    input reset;
+    output reg [3:0] count;
+
+
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            count <= 4'b1111;
+        else
+            count <= count - 1;
+    end
+
+endmodule
